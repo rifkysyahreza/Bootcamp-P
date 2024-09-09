8066177e6257d458b30ba3af362b5c7f986ecfb8
Rifky Syahreza <rsyahrezafs@gmail.com>
2024-09-09 16:51:21 +0700

    adding a comment

--- Day 6 Exercise/src/main/java/org/example/NumberOfWaitingDays.java
+++ Day 6 Exercise/src/main/java/org/example/NumberOfWaitingDays.java
@@ -9,18 +9,24 @@ public class NumberOfWaitingDays {
         int days = 1;
         for (int i = 0; i < arrayOfDegree.length; i++){
 
+            // Check is this iteration is the last iteration
             if( i == arrayOfDegree.length - 1){
                 listOfWaitingDays.add(0);
             }
 
             for (int j = i +1; j < arrayOfDegree.length; j++){
+
+                // Check if the next value is bigger than the current value
                 if (arrayOfDegree[i] < arrayOfDegree[j]){
                     listOfWaitingDays.add(days);
                     days = 1;
                     break;
                 }
 
+                // Check if the next value is lower than the current value
                 if (arrayOfDegree[i] > arrayOfDegree[j]){
+
+                    // Check if this value is bigger than next value and this iteration is the last iteration
                     if (j == arrayOfDegree.length - 1){
                         listOfWaitingDays.add(0);
                     }
